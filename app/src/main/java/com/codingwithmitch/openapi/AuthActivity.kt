46d78420e5ca6d565ada8a61f5e077bf82ce87dc
Mitch Tabian <mitch@tabian.ca>
2019-09-13 15:27:59 -0700

    starting point

---
+++ app/src/main/java/com/codingwithmitch/openapi/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.codingwithmitch.openapi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
--- app/src/main/java/com/codingwithmitch/openapi/ui/UICommunicationListener.kt
+++
@@ -1,6 +0,0 @@
-package com.codingwithmitch.openapi.ui
-
-interface UICommunicationListener {
-
-    fun onUIMessageReceived(uiMessage: UIMessage)
-}
