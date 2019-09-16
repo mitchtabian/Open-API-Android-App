1fa993f7091f461f6860d6331c11cd7f865cb613
Mitch Tabian <mitch@tabian.ca>
2019-09-16 15:36:44 -0700

    Room persistence setup

---
+++ app/src/main/java/com/codingwithmitch/openapi/models/AccountProperties.kt
@@ -0,0 +1,39 @@
+package com.codingwithmitch.openapi.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "account_properties")
+data class AccountProperties(
+
+    @SerializedName("pk")
+    @Expose
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "pk") var pk: Int,
+
+    @SerializedName("email")
+    @Expose
+    @ColumnInfo(name = "email") var email: String,
+
+    @SerializedName("username")
+    @Expose
+    @ColumnInfo(name = "username") var username: String
+)
+{
+
+    override fun equals(other: Any?): Boolean {
+        if (javaClass != other?.javaClass) return false
+
+        other as AccountProperties
+
+        if (pk != other.pk) return false
+        if (email != other.email) return false
+        if (username != other.username) return false
+
+        return true
+    }
+
+}
---
+++ app/src/main/java/com/codingwithmitch/openapi/models/AuthToken.kt
@@ -0,0 +1,43 @@
+package com.codingwithmitch.openapi.models
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+/**
+ * Data class for saving authentication token locally for open-api.xyz
+ * NOTES:
+ * 1) local 'auth_token' table has foreign key relationship to 'account_properties' table through 'account' field (PK)
+ *
+ * Docs: https://open-api.xyz/api/
+ */
+@Entity(
+    tableName = "auth_token",
+    foreignKeys = [
+        ForeignKey(
+            entity = AccountProperties::class,
+            parentColumns = ["pk"],
+            childColumns = ["account_pk"],
+            onDelete = CASCADE
+        )
+    ]
+)
+@Parcelize
+data class AuthToken(
+
+    @PrimaryKey
+    @ColumnInfo(name = "account_pk")
+    var account_pk: Int? = -1,
+
+
+    @ColumnInfo(name = "token")
+    @SerializedName("token")
+    @Expose
+    var token: String? = null
+) : Parcelable
