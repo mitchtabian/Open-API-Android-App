e645949c08148c17e0310df501ecf66c555da9c1
Mitch Tabian <mitch@tabian.ca>
2019-09-23 11:05:15 -0700

    Build NetworkBoundResource

--- app/src/main/java/com/codingwithmitch/openapi/util/Constants.kt
+++ app/src/main/java/com/codingwithmitch/openapi/util/Constants.kt
@@ -5,5 +5,9 @@ class Constants {
     companion object{
 
         const val BASE_URL = "https://open-api.xyz/api/"
+
+        const val NETWORK_TIMEOUT = 3000L
+        const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
+        const val TESTING_CACHE_DELAY = 0L // fake cache delay for testing
     }
 }
--- app/src/main/java/com/codingwithmitch/openapi/util/ErrorHandling.kt
+++ app/src/main/java/com/codingwithmitch/openapi/util/ErrorHandling.kt
@@ -4,6 +4,17 @@ class ErrorHandling {
 
     companion object{
 
+        const val UNABLE_TO_RESOLVE_HOST = "Unable to resolve host"
+        const val UNABLE_TODO_OPERATION_WO_INTERNET = "Can't do that operation without an internet connection"
+        const val ERROR_CHECK_NETWORK_CONNECTION = "Check network connection."
+
         const val ERROR_UNKNOWN = "Unknown error"
+
+        fun isNetworkError(msg: String): Boolean{
+            when{
+                msg.contains(UNABLE_TO_RESOLVE_HOST) -> return true
+                else-> return false
+            }
+        }
     }
 }
