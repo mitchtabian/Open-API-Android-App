3b41ff0fb9fa0974b5b987071f1ae7f2d4c99e1e
Mitch Tabian <mitch@tabian.ca>
2019-09-30 10:27:38 -0700

    Add main BaseFragments

---
+++ app/src/main/java/com/codingwithmitch/openapi/ui/main/create_blog/BaseCreateBlogFragment.kt
@@ -0,0 +1,33 @@
+package com.codingwithmitch.openapi.ui.main.create_blog
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.codingwithmitch.openapi.ui.DataStateChangeListener
+import com.codingwithmitch.openapi.viewmodels.ViewModelProviderFactory
+import dagger.android.support.DaggerFragment
+import javax.inject.Inject
+
+class BaseCreateBlogFragment : DaggerFragment(){
+
+    val TAG: String = "AppDebug"
+
+    @Inject
+    lateinit var providerFactory: ViewModelProviderFactory
+
+    lateinit var stateChangeListener: DataStateChangeListener
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try{
+            stateChangeListener = context as DataStateChangeListener
+        }catch(e: ClassCastException){
+            Log.e(TAG, "$context must implement DataStateChangeListener" )
+        }
+    }
+}
