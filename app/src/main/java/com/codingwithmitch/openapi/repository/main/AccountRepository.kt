0dace6cd3c0c340d9316d0674dc00badd09590fc
Mitch Tabian <mitch@tabian.ca>
2019-10-04 10:16:50 -0700

    Caching Account Properties

--- app/src/main/java/com/codingwithmitch/openapi/repository/main/AccountRepository.kt
+++ app/src/main/java/com/codingwithmitch/openapi/repository/main/AccountRepository.kt
@@ -1,8 +1,18 @@
 package com.codingwithmitch.openapi.repository.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import com.codingwithmitch.openapi.api.main.OpenApiMainService
+import com.codingwithmitch.openapi.models.AccountProperties
+import com.codingwithmitch.openapi.models.AuthToken
 import com.codingwithmitch.openapi.persistence.AccountPropertiesDao
+import com.codingwithmitch.openapi.repository.NetworkBoundResource
 import com.codingwithmitch.openapi.session.SessionManager
+import com.codingwithmitch.openapi.ui.DataState
+import com.codingwithmitch.openapi.ui.main.account.state.AccountViewState
+import com.codingwithmitch.openapi.util.ApiSuccessResponse
+import com.codingwithmitch.openapi.util.GenericApiResponse
+import kotlinx.coroutines.Job
 import javax.inject.Inject
 
 class AccountRepository
@@ -14,71 +24,47 @@ constructor(
 )
 {
 
+    private val TAG: String = "AppDebug"
+
+    private var repositoryJob: Job? = null
+
     fun getAccountProperties(authToken: AuthToken): LiveData<DataState<AccountViewState>> {
-        return object: NetworkBoundResource<AccountProperties, AccountProperties, AccountViewState>(
-            "getAccountProperties",
+        return object: NetworkBoundResource<AccountProperties, AccountViewState>(
             sessionManager.isConnectedToTheInternet(),
-            true,
-            false,
             true
         ){
 
             // if network is down, view the cache and return
             override suspend fun createCacheRequestAndReturn() {
-                withContext(Dispatchers.Main){
 
-                    // finishing by viewing db cache
-                    result.addSource(loadFromCache()){ viewState ->
-                        onCompleteJob(DataState.data(viewState, null))
-                    }
-                }
             }
 
             override suspend fun handleApiSuccessResponse(response: ApiSuccessResponse<AccountProperties>) {
-                updateLocalDb(response.body)
 
-                withContext(Dispatchers.Main){
 
-                    // finishing by viewing db cache
-                    result.addSource(loadFromCache()){ viewState ->
-                        onCompleteJob(DataState.data(viewState, null))
-                    }
-                }
             }
 
-            override fun loadFromCache(): LiveData<AccountViewState> {
-
-                return accountPropertiesDao.searchByPk(authToken.account_pk!!)
-                    .switchMap {
-                        object: LiveData<AccountViewState>(){
-                            override fun onActive() {
-                                super.onActive()
-                                value = AccountViewState(it)
-                            }
-                        }
-                    }
-            }
 
             override fun createCall(): LiveData<GenericApiResponse<AccountProperties>> {
-                return openApiMainService.getAccountProperties("Token ${authToken.token!!}")
+                return openApiMainService
+                    .getAccountProperties(
+                        "Token ${authToken.token!!}"
+                    )
             }
 
-            override suspend fun updateLocalDb(accountProp: AccountProperties?) {
-                accountProp?.let {
-                    accountPropertiesDao.updateAccountProperties(
-                        accountProp.pk,
-                        accountProp.email,
-                        accountProp.username
-                    )
-                }
-            }
 
             override fun setJob(job: Job) {
-                jobManager.addJob(methodName, job)
+                repositoryJob?.cancel()
+                repositoryJob = job
             }
 
 
         }.asLiveData()
+    }
+
+    fun cancelActiveJobs(){
+        Log.d(TAG, "AuthRepository: Cancelling on-going jobs...")
+        repositoryJob?.cancel()
     }
 
 }
