ab46457cafcfa0b3c84f87528fc4ceb61d81b2c7
Mitch Tabian <mitch@tabian.ca>
2019-10-04 10:48:17 -0700

    Caching Account Properties

--- app/src/main/java/com/codingwithmitch/openapi/ui/main/account/AccountFragment.kt
+++ app/src/main/java/com/codingwithmitch/openapi/ui/main/account/AccountFragment.kt
@@ -1,12 +1,14 @@
 package com.codingwithmitch.openapi.ui.main.account
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.codingwithmitch.openapi.R
-import com.codingwithmitch.openapi.session.SessionManager
+import com.codingwithmitch.openapi.models.AccountProperties
+import com.codingwithmitch.openapi.ui.main.account.state.AccountStateEvent
 import kotlinx.android.synthetic.main.fragment_account.*
-import javax.inject.Inject
 
 class AccountFragment : BaseAccountFragment(){
 
@@ -29,6 +31,45 @@ class AccountFragment : BaseAccountFragment(){
         logout_button.setOnClickListener {
             viewModel.logout()
         }
+
+        subscribeObservers()
+    }
+
+    private fun subscribeObservers(){
+        viewModel.dataState.observe(viewLifecycleOwner, Observer{ dataState ->
+            stateChangeListener.onDataStateChange(dataState)
+            if(dataState != null){
+                dataState.data?.let { data ->
+                    data.data?.let{ event ->
+                        event.getContentIfNotHandled()?.let{ viewState ->
+                            viewState.accountProperties?.let{ accountProperties ->
+                                Log.d(TAG, "AccountFragment, DataState: ${accountProperties}")
+                                viewModel.setAccountPropertiesData(accountProperties)
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        viewModel.viewState.observe(viewLifecycleOwner, Observer{ viewState->
+            if(viewState != null){
+                viewState.accountProperties?.let{
+                    Log.d(TAG, "AccountFragment, ViewState: ${it}")
+                    setAccountDataFields(it)
+                }
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.setStateEvent(AccountStateEvent.GetAccountPropertiesEvent())
+    }
+
+    private fun setAccountDataFields(accountProperties: AccountProperties){
+        email?.setText(accountProperties.email)
+        username?.setText(accountProperties.username)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
