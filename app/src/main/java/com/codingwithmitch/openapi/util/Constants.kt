c73ff0f911f97bb93617b461600df75a1b74f067
Mitch Tabian <mitch@tabian.ca>
2019-10-04 16:08:21 -0700

    remove fake network delay

--- app/src/main/java/com/codingwithmitch/openapi/util/Constants.kt
+++ app/src/main/java/com/codingwithmitch/openapi/util/Constants.kt
@@ -8,7 +8,7 @@ class Constants {
         const val PASSWORD_RESET_URL: String = "https://open-api.xyz/password_reset/"
 
         const val NETWORK_TIMEOUT = 3000L
-        const val TESTING_NETWORK_DELAY = 2000L // fake network delay for testing
+        const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
         const val TESTING_CACHE_DELAY = 0L // fake cache delay for testing
     }
 }
