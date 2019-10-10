34717ec35fee38e018b3ec4cd23e30f060e32061
Mitch Tabian <mitch@tabian.ca>
2019-10-10 11:46:12 -0700

    fix job cancel bug

--- app/src/main/java/com/codingwithmitch/openapi/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/codingwithmitch/openapi/ui/auth/AuthViewModel.kt
@@ -41,7 +41,14 @@ constructor(
                 return authRepository.checkPreviousAuthUser()
             }
 
-
+            is None ->{
+                return object: LiveData<DataState<AuthViewState>>(){
+                    override fun onActive() {
+                        super.onActive()
+                        value = DataState.data(null, null)
+                    }
+                }
+            }
         }
     }
 
@@ -77,9 +84,13 @@ constructor(
     }
 
     fun cancelActiveJobs(){
+        handlePendingData()
         authRepository.cancelActiveJobs()
     }
 
+    fun handlePendingData(){
+        setStateEvent(None())
+    }
 
     override fun onCleared() {
         super.onCleared()
