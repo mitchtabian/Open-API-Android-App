18251c99c996c0b4e87f53da3f945ff7a843c866
Mitch Tabian <mitch@tabian.ca>
2019-10-15 16:17:21 -0700

    RecyclerView Adapter, DiffUtil and ListUpdateCallback

---
+++ app/src/main/java/com/codingwithmitch/openapi/ui/main/blog/BlogListAdapter.kt
@@ -0,0 +1,75 @@
+package com.codingwithmitch.openapi.ui.main.blog
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import com.codingwithmitch.openapi.R
+import com.codingwithmitch.openapi.models.BlogPost
+
+class BlogListAdapter(private val interaction: Interaction? = null) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    val DIFF_CALLBACK = object : DiffUtil.ItemCallback<BlogPost>() {
+
+        override fun areItemsTheSame(oldItem: BlogPost, newItem: BlogPost): Boolean {
+            TODO("not implemented")
+        }
+
+        override fun areContentsTheSame(oldItem: BlogPost, newItem: BlogPost): Boolean {
+            TODO("not implemented")
+        }
+
+    }
+    private val differ = AsyncListDiffer(this, DIFF_CALLBACK)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return BlogViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.layout_blog_list_item,
+                parent,
+                false
+            ),
+            interaction
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is BlogViewHolder -> {
+                holder.bind(differ.currentList.get(position))
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    fun submitList(list: List<BlogPost>) {
+        differ.submitList(list)
+    }
+
+    class BlogViewHolder
+    constructor(
+        itemView: View,
+        private val interaction: Interaction?
+    ) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(item: BlogPost) = with(itemView) {
+            itemView.setOnClickListener {
+                interaction?.onItemSelected(adapterPosition, item)
+            }
+
+            TODO("bind view with data")
+        }
+    }
+
+    interface Interaction {
+        fun onItemSelected(position: Int, item: BlogPost)
+    }
+}
