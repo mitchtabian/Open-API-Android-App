8af6b448d203a01373d2188bb21fd2db22b2f362
Mitch Tabian <mitch@tabian.ca>
2019-10-15 17:36:38 -0700

    Pagination and Exhausted Queries

--- app/src/main/java/com/codingwithmitch/openapi/persistence/BlogPostDao.kt
+++ app/src/main/java/com/codingwithmitch/openapi/persistence/BlogPostDao.kt
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.codingwithmitch.openapi.models.BlogPost
+import com.codingwithmitch.openapi.util.Constants.Companion.PAGINATION_PAGE_SIZE
 
 @Dao
 interface BlogPostDao {
@@ -15,6 +16,18 @@ interface BlogPostDao {
 
     @Query("SELECT * FROM blog_post")
     fun getAllBlogPosts(): LiveData<List<BlogPost>>
+
+    @Query("SELECT * FROM blog_post WHERE title LIKE '%' || :query || '%' OR body LIKE '%' || :query || '%' OR username LIKE '%' || :query || '%' ORDER BY date_updated DESC LIMIT (:page * :pageSize)")
+    fun searchBlogPostsOrderByDateDESC(query: String, page: Int, pageSize: Int = PAGINATION_PAGE_SIZE): LiveData<List<BlogPost>>
+
+    @Query("SELECT * FROM blog_post WHERE title LIKE '%' || :query || '%' OR body LIKE '%' || :query || '%' OR username LIKE '%' || :query || '%' ORDER BY date_updated  ASC LIMIT (:page * :pageSize)")
+    fun searchBlogPostsOrderByDateASC(query: String, page: Int, pageSize: Int = PAGINATION_PAGE_SIZE): LiveData<List<BlogPost>>
+
+    @Query("SELECT * FROM blog_post WHERE title LIKE '%' || :query || '%' OR body LIKE '%' || :query || '%' OR username LIKE '%' || :query || '%' ORDER BY username DESC LIMIT (:page * :pageSize)")
+    fun searchBlogPostsOrderByAuthorDESC(query: String, page: Int, pageSize: Int = PAGINATION_PAGE_SIZE): LiveData<List<BlogPost>>
+
+    @Query("SELECT * FROM blog_post WHERE title LIKE '%' || :query || '%' OR body LIKE '%' || :query || '%' OR username LIKE '%' || :query || '%' ORDER BY username  ASC LIMIT (:page * :pageSize)")
+    fun searchBlogPostsOrderByAuthorASC(query: String, page: Int, pageSize: Int = PAGINATION_PAGE_SIZE): LiveData<List<BlogPost>>
 }
 
 
--- app/src/main/java/com/codingwithmitch/openapi/util/Constants.kt
+++ app/src/main/java/com/codingwithmitch/openapi/util/Constants.kt
@@ -10,5 +10,7 @@ class Constants {
         const val NETWORK_TIMEOUT = 6000L
         const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
         const val TESTING_CACHE_DELAY = 0L // fake cache delay for testing
+
+        const val PAGINATION_PAGE_SIZE = 10
     }
 }
