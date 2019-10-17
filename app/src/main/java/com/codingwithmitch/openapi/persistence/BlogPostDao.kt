0f85cd3b7920d3c2fe993a8aab33dbf82626f854
Mitch Tabian <mitch@tabian.ca>
2019-10-17 08:52:00 -0700

    cleanup sql query

--- app/src/main/java/com/codingwithmitch/openapi/persistence/BlogPostDao.kt
+++ app/src/main/java/com/codingwithmitch/openapi/persistence/BlogPostDao.kt
@@ -14,9 +14,18 @@ interface BlogPostDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(blogPost: BlogPost): Long
 
-    @Query("SELECT * FROM blog_post WHERE title LIKE '%' || :query || '%' OR body LIKE '%' || :query || '%' OR username LIKE '%' || :query || '%'  LIMIT (:page * :pageSize)")
-    fun getAllBlogPosts(query: String, page: Int, pageSize: Int = PAGINATION_PAGE_SIZE): LiveData<List<BlogPost>>
-
+    @Query("""
+        SELECT * FROM blog_post 
+        WHERE title LIKE '%' || :query || '%' 
+        OR body LIKE '%' || :query || '%' 
+        OR username LIKE '%' || :query || '%' 
+        LIMIT (:page * :pageSize)
+        """)
+    fun getAllBlogPosts(
+        query: String,
+        page: Int,
+        pageSize: Int = PAGINATION_PAGE_SIZE
+    ): LiveData<List<BlogPost>>
 }
 
 
