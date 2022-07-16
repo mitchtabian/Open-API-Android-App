1415271bc6e81be7b4e3e2864f2d4d218fa1d381
luna-zivkovic <luna.zivkovic@uvc-solutions.com>
2022-07-16 12:00:34 +0200

    changes

--- app/src/main/java/com/templateapp/cloudapi/business/domain/util/Constants.kt
+++ app/src/main/java/com/templateapp/cloudapi/business/domain/util/Constants.kt
@@ -6,8 +6,8 @@ class Constants {
 
         //const val BASE_URL = "http://appcloud-env.eba-theyd4uu.eu-central-1.elasticbeanstalk.com/"
         //const val PASSWORD_RESET_URL: String = "http://appcloud-env.eba-theyd4uu.eu-central-1.elasticbeanstalk.com/passwordresetform"
-        const val BASE_URL = "http://192.168.64.149:3000/"
-        const val PASSWORD_RESET_URL: String = "http://192.168.64.149:3000/passwordresetform"
+        const val BASE_URL = "http://192.168.1.10:3000/"
+        const val PASSWORD_RESET_URL: String = "http://192.168.1.10:3000/passwordresetform"
         //const val BASE_URL = "http://127.0.0.1:3000/"
          //const val PASSWORD_RESET_URL: String = "http://127.0.0.1:3000/passwordresetform"
 
